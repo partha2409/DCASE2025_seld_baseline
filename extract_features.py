b25a1b1930ece251b1d16a614a80b6ecc11c3ab8
Parthasaarathy <44229561+partha2409@users.noreply.github.com>
2025-02-17 15:52:29 +0200

    initial commit

--- extract_features.py
+++ extract_features.py
@@ -2,30 +2,28 @@
 extract_features.py
 
 This module defines the SELDFeatureExtractor class, which provides functionality to extract features from both audio
-and video data. It includes the following key components:
+and video data. It also processes the labels to support MultiACCDOA (ADPIT).  It includes the following key components:
 
 Classes:
-    SELDFeatureExtractor: A class that supports the extraction of audio and video features.
-    It can extract log Mel spectrogram from audio files and ResNet-based features from video frames.
+    SELDFeatureExtractor: A class that supports the extraction of audio and video features. It extracts log Mel
+    spectrogram from audio files and ResNet-based features from video frames. It also processes labels for MultiACCDOA.
 
     Methods:
         - extract_audio_features: Extracts audio features from a specified split of the dataset.
         - extract_video_features: Extracts video features from a specified split of the dataset.
         - extract_features: A high-level function to extract features based on the modality ('audio' or 'audio_visual').
+        - extract_labels: converts labels to support multiACCDOA.
 
 Author: Parthasaarathy Sudarsanam, Audio Research Group, Tampere University
-Date: January 2025
+Date: February 2025
 """
+
 import os
-import librosa
-import librosa.feature
 import glob
-import numpy as np
-import cv2
 import torch
 from torchvision.models import resnet50, ResNet50_Weights
-from PIL import Image
 from tqdm import tqdm
+import utils
 
 
 class SELDFeatureExtractor():
@@ -59,6 +57,10 @@ class SELDFeatureExtractor():
             self.backbone.eval()
             self.preprocess = self.weights.transforms()
 
+        # label extraction
+        self.nb_label_frames = params['label_sequence_length']
+        self.nb_unique_classes = params['nb_classes']
+
     def extract_audio_features(self, split):
         """
         Extracts audio features for a given split (dev/eval).
@@ -82,8 +84,8 @@ class SELDFeatureExtractor():
             if os.path.exists(feature_path):
                 continue
             # If the feature file doesn't exist, perform extraction
-            audio, sr = self.load_audio(audio_file)
-            audio_feat = self.extract_log_mel_spectrogram(audio, sr)
+            audio, sr = utils.load_audio(audio_file, self.sampling_rate)
+            audio_feat = utils.extract_log_mel_spectrogram(audio, sr, self.n_fft, self.hop_length, self.win_length, self.nb_mels)
             audio_feat = torch.tensor(audio_feat, dtype=torch.float32)
             torch.save(audio_feat, feature_path)
 
@@ -111,8 +113,8 @@ class SELDFeatureExtractor():
                 continue
 
             # If the feature file doesn't exist, perform extraction
-            video_frames = self.load_video(video_file)
-            video_feat = self.extract_resnet_features(video_frames)
+            video_frames = utils.load_video(video_file, self.fps)
+            video_feat = utils.extract_resnet_features(video_frames, self.preprocess, self.backbone, self.device)
             torch.save(video_feat, feature_path)
 
     def extract_features(self, split='dev'):
@@ -132,84 +134,34 @@ class SELDFeatureExtractor():
         else:
             raise ValueError("Modality should be one of 'audio' or 'audio_visual'. You can set the modality in params.py")
 
-    def load_audio(self, audio_file):
-        """
-        Loads an audio file.
-        Args:
-            audio_file (str): Path to the audio file.
-        Returns:
-            tuple: (audio_data, sample_rate)
-        """
+    def extract_labels(self, split):
 
-        audio_data, sr = librosa.load(path=audio_file, sr=self.sampling_rate, mono=False)
-        return audio_data, sr
+        os.makedirs(self.feat_dir, exist_ok=True)   # already created by extract_features method
 
-    def extract_log_mel_spectrogram(self, audio, sr):
-        """
-        Extracts log mel spectrogram from audio data.
-        Args:
-            audio (ndarray): Audio data.
-            sr (int): Sample rate of the audio.
-        Returns:
-            ndarray: Log mel spectrogram.
-        """
+        if split == 'dev':
+            label_files = glob.glob(os.path.join(self.root_dir, 'metadata_dev', 'dev-*', '*.csv'))
+        elif split == 'eval':  # only for organizers
+            label_files = glob.glob(os.path.join(self.root_dir, 'metadata_eval', 'eval', '*.csv'))
+        else:
+            raise ValueError("Split must be either 'dev' or 'eval'.")
 
-        linear_stft = librosa.stft(y=audio, n_fft=self.n_fft, hop_length=self.hop_length, win_length=self.win_length).T
-        linear_stft_mag = np.abs(linear_stft) ** 2
-        mel_spec = librosa.feature.melspectrogram(S=linear_stft_mag, sr=sr, n_mels=self.nb_mels)
-        log_mel_spectrogram = librosa.power_to_db(mel_spec)
-        log_mel_spectrogram = log_mel_spectrogram.transpose((0, 2, 1)).reshape(linear_stft.shape[0], -1)
-        return log_mel_spectrogram
+        os.makedirs(os.path.join(self.feat_dir, f'metadata_{split}'), exist_ok=True)
 
-    def load_video(self, video_file):
-        """
-        Loads video frames from a video file.
-        Args:
-            video_file (str): Path to the video file.
-        Returns:
-            list: List of PIL images (frames).
-        """
+        for label_file in tqdm(label_files, desc=f"Processing label files ({split})", unit="file"):
+            filename = os.path.splitext(os.path.basename(label_file))[0] + '.pt'
+            feature_path = os.path.join(self.feat_dir, f'metadata_{split}', filename)
 
-        cap = cv2.VideoCapture(video_file)
-        video_fps = int(cap.get(cv2.CAP_PROP_FPS))
-        frame_interval = max(1, video_fps // self.fps)
+            # Check if the feature file already exists
+            if os.path.exists(feature_path):
+                continue
 
-        pil_frames = []
-        frame_cnt = 0
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
-            if frame_cnt % frame_interval == 0:
-                resized_frame = cv2.resize(frame, (360, 180))
-                frame_rgb = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
-                pil_frame = Image.fromarray(frame_rgb)
-                pil_frames.append(pil_frame)
-            frame_cnt += 1
-        cap.release()
-        return pil_frames
-
-    def extract_resnet_features(self, video_frames):
-        """
-        Extracts ResNet-50 features from video frames.
-        Args:
-            video_frames (list): List of PIL video frames.
-        Returns:
-            ndarray: Extracted video features.
-        """
-
-        with torch.no_grad():
-            preprocessed_images = [self.preprocess(image) for image in video_frames]
-            max_batch_size = 1000
-            iter = (len(preprocessed_images) - 1) // max_batch_size + 1
-            vid_features_part_list = []
-            for i in range(iter):
-                preprocessed_images_part = torch.stack(preprocessed_images[i * max_batch_size: (i + 1) * max_batch_size], dim=0).to(self.device)
-                vid_features_part = self.backbone(preprocessed_images_part)
-                vid_features_part = torch.mean(vid_features_part, dim=1)
-                vid_features_part_list.append(vid_features_part)
-            vid_features = torch.cat(vid_features_part_list, dim=0)
-        return vid_features
+            # If the feature file doesn't exist, perform extraction
+            label_data = utils.load_labels(label_file)
+            if params['multiACCDOA']:  # TODO: currently multiaccdoa is always set to True
+                processed_labels = utils.get_adpit_labels(label_data, self.nb_label_frames, self.nb_unique_classes)
+            else:
+                raise NotImplementedError("This code currently supports only MultiACCDOA configuration. Set it to True.")
+            torch.save(processed_labels, feature_path)
 
 
 if __name__ == '__main__':
@@ -217,6 +169,7 @@ if __name__ == '__main__':
     # All the classes will be called from the main.py for actual use.
     from parameters import params
     feature_extractor = SELDFeatureExtractor(params)
-    feature_extractor.extract_features(split='dev')
+    #feature_extractor.extract_features(split='dev')
+    feature_extractor.extract_labels(split='dev')
 
 
--- utils.py
+++ utils.py
@@ -2,8 +2,291 @@
 utils.py
 
 This module includes miscellaneous utility functions that support the project,
-such as logging, file handling, and general-purpose helpers.
+such as data_preprocessing, logging, file handling, and general-purpose helpers.
 
 Author: Parthasaarathy Sudarsanam, Audio Research Group, Tampere University
-Date: January 2025
+Date: February 2025
 """
+
+import os
+from torch.utils.tensorboard import SummaryWriter
+import time
+import pickle
+import librosa
+import librosa.feature
+import numpy as np
+import cv2
+from PIL import Image
+import torch
+
+
+def setup(params):
+    """
+    Sets up the environment for training by creating directories for model checkpoints
+    and logging, saving configuration parameters, and initializing a tensorboard summary writer.
+    Args:
+        params (dict): Dictionary containing the configuration parameters.
+    Returns:
+        tuple: A tuple containing the path to the checkpoints folder and the tensorboard summary writer instance.
+    """
+    # create dir to save model checkpoints
+    reference = params['net_type'] + '_' + params['modality'] + str(time.strftime('_%Y%m%d_%H%M%S'))
+    checkpoints_folder = os.path.join(params['output_folder'] + 'checkpoints' + reference)
+    os.makedirs(checkpoints_folder, exist_ok=True)
+
+    # save the all the config/hyperparams to a pickle file
+    pickle_filepath = os.path.join(str(checkpoints_folder), 'config.pkl')
+    pickle_file = open(pickle_filepath, 'wb')
+    pickle.dump(params, pickle_file)
+
+    # create a tensorboard summary writer for logging and visualization
+    log_dir = os.path.join(params['log_dir'], reference)
+    os.makedirs(log_dir, exist_ok=True)
+    summary_writer = SummaryWriter(log_dir=str(log_dir))
+
+    return checkpoints_folder, summary_writer
+
+
+def load_audio(audio_file, sampling_rate):
+    """
+    Loads an audio file.
+    Args:
+        audio_file (str): Path to the audio file.
+        sampling_rate (int): Target sampling rate
+    Returns:
+        tuple: (audio_data, sample_rate)
+    """
+    audio_data, sr = librosa.load(path=audio_file, sr=sampling_rate, mono=False)
+    return audio_data, sr
+
+
+def extract_log_mel_spectrogram(audio, sr, n_fft, hop_length, win_length, nb_mels):
+    """
+    Computes the log Mel spectrogram from an audio signal.
+
+    Parameters:
+        audio (ndarray): NumPy array containing the audio waveform.
+        sr (int): The sample rate of the audio signal.
+        n_fft (int): Size of the FFT window.
+        hop_length (int): Number of samples to shift between successive frames.
+        win_length (int): Length of each windowed frame in samples.
+        nb_mels (int): Number of Mel filter banks to use.
+
+    Returns:
+        ndarray: Array of shape (2, time_frames, nb_mels) - log Mel spectrogram for each channel.
+    """
+
+    linear_stft = librosa.stft(y=audio, n_fft=n_fft, hop_length=hop_length, win_length=win_length).T
+    linear_stft_mag = np.abs(linear_stft) ** 2
+    mel_spec = librosa.feature.melspectrogram(S=linear_stft_mag, sr=sr, n_mels=nb_mels)
+    log_mel_spectrogram = librosa.power_to_db(mel_spec)
+    log_mel_spectrogram = log_mel_spectrogram.transpose((2, 0, 1))
+    return log_mel_spectrogram
+
+
+def load_video(video_file, fps):
+    """
+    Loads video frames from a video file.
+    Args:
+        video_file (str): Path to the video file.
+        fps (int): Target frames per second
+    Returns:
+        list: List of PIL images (frames).
+    """
+
+    cap = cv2.VideoCapture(video_file)
+    video_fps = 30
+    frame_interval = max(1, video_fps // fps)
+    pil_frames = []
+    frame_cnt = 0
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if frame_cnt % frame_interval == 0:  # smoothening may not be required since we process the frames individually through a resnet
+            resized_frame = cv2.resize(frame, (360, 180))
+            frame_rgb = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
+            pil_frame = Image.fromarray(frame_rgb)
+            pil_frames.append(pil_frame)
+        frame_cnt += 1
+    cap.release()
+    return pil_frames
+
+
+def extract_resnet_features(video_frames, resnet_preprocessor, resnet_backbone, device):
+    """
+    Extracts ResNet-50 features from video frames.
+    Args:
+        video_frames (list): List of PIL video frames.
+        resnet_preprocessor (callable): Preprocessing function to prepare images for ResNet input.
+        resnet_backbone (torch.nn.Module): Pre-trained ResNet model to extract features.
+        device (str): Device to perform computation on (e.g., 'cuda' or 'cpu').
+    Returns:
+        tensor: Extracted video features of shape (nb_frames, 7 ,7)
+    """
+
+    with torch.no_grad():
+        preprocessed_images = [resnet_preprocessor(image) for image in video_frames]
+        preprocessed_images = torch.stack(preprocessed_images, dim=0).to(device)
+        vid_features = resnet_backbone(preprocessed_images)
+        vid_features = torch.mean(vid_features, dim=1)
+        return vid_features
+
+
+def load_labels(label_file, convert_to_cartesian=True):
+    label_data = {}
+    with open(label_file, 'r') as file:
+        lines = file.readlines()[1:]  # Skip the header
+        for line in lines:
+            values = line.strip().split(',')
+            frame_idx = int(values[0])
+            data_row = [int(values[1]), int(values[2]), float(values[3]), float(values[4]), int(values[5])]
+            if frame_idx not in label_data:
+                label_data[frame_idx] = []
+            label_data[frame_idx].append(data_row)
+
+    if convert_to_cartesian:
+        label_data = convert_polar_to_cartesian(label_data)
+    return label_data
+
+
+def get_adpit_labels(_desc_file, _nb_label_frames, _nb_unique_classes):
+
+    se_label = np.zeros((_nb_label_frames, 6, _nb_unique_classes))  # 50, 6, 13
+    x_label = np.zeros((_nb_label_frames, 6, _nb_unique_classes))
+    y_label = np.zeros((_nb_label_frames, 6, _nb_unique_classes))
+    dist_label = np.zeros((_nb_label_frames, 6, _nb_unique_classes))
+    onscreen_label = np.zeros((_nb_label_frames, 6, _nb_unique_classes))
+
+    for frame_ind, active_event_list in _desc_file.items():
+        if frame_ind < _nb_label_frames:
+            active_event_list.sort(key=lambda x: x[0])  # sort for ov from the same class
+            active_event_list_per_class = []
+            for i, active_event in enumerate(active_event_list):
+                active_event_list_per_class.append(active_event)
+                if i == len(active_event_list) - 1:  # if the last
+                    if len(active_event_list_per_class) == 1:  # if no ov from the same class
+                        # a0----
+                        active_event_a0 = active_event_list_per_class[0]
+                        se_label[frame_ind, 0, active_event_a0[0]] = 1
+                        x_label[frame_ind, 0, active_event_a0[0]] = active_event_a0[2]
+                        y_label[frame_ind, 0, active_event_a0[0]] = active_event_a0[3]
+                        dist_label[frame_ind, 0, active_event_a0[0]] = active_event_a0[4] / 100.
+                        onscreen_label[frame_ind, 0, active_event_a0[0]] = active_event_a0[5]
+                    elif len(active_event_list_per_class) == 2:  # if ov with 2 sources from the same class
+                        # --b0--
+                        active_event_b0 = active_event_list_per_class[0]
+                        se_label[frame_ind, 1, active_event_b0[0]] = 1
+                        x_label[frame_ind, 1, active_event_b0[0]] = active_event_b0[2]
+                        y_label[frame_ind, 1, active_event_b0[0]] = active_event_b0[3]
+                        dist_label[frame_ind, 1, active_event_b0[0]] = active_event_b0[4] / 100.
+                        onscreen_label[frame_ind, 0, active_event_b0[0]] = active_event_b0[5]
+                        # --b1--
+                        active_event_b1 = active_event_list_per_class[1]
+                        se_label[frame_ind, 2, active_event_b1[0]] = 1
+                        x_label[frame_ind, 2, active_event_b1[0]] = active_event_b1[2]
+                        y_label[frame_ind, 2, active_event_b1[0]] = active_event_b1[3]
+                        dist_label[frame_ind, 2, active_event_b1[0]] = active_event_b1[4] / 100.
+                        onscreen_label[frame_ind, 2, active_event_b1[0]] = active_event_b1[5]
+
+                    else:  # if ov with more than 2 sources from the same class
+                        # ----c0
+                        active_event_c0 = active_event_list_per_class[0]
+                        se_label[frame_ind, 3, active_event_c0[0]] = 1
+                        x_label[frame_ind, 3, active_event_c0[0]] = active_event_c0[2]
+                        y_label[frame_ind, 3, active_event_c0[0]] = active_event_c0[3]
+                        dist_label[frame_ind, 3, active_event_c0[0]] = active_event_c0[4] / 100.
+                        onscreen_label[frame_ind, 3, active_event_c0[0]] = active_event_c0[5]
+
+                        # ----c1
+                        active_event_c1 = active_event_list_per_class[1]
+                        se_label[frame_ind, 4, active_event_c1[0]] = 1
+                        x_label[frame_ind, 4, active_event_c1[0]] = active_event_c1[2]
+                        y_label[frame_ind, 4, active_event_c1[0]] = active_event_c1[3]
+                        dist_label[frame_ind, 4, active_event_c1[0]] = active_event_c1[4] / 100.
+                        onscreen_label[frame_ind, 4, active_event_c1[0]] = active_event_c1[5]
+                        # ----c2
+                        active_event_c2 = active_event_list_per_class[2]
+                        se_label[frame_ind, 5, active_event_c2[0]] = 1
+                        x_label[frame_ind, 5, active_event_c2[0]] = active_event_c2[2]
+                        y_label[frame_ind, 5, active_event_c2[0]] = active_event_c2[3]
+                        dist_label[frame_ind, 5, active_event_c2[0]] = active_event_c2[4] / 100.
+                        onscreen_label[frame_ind, 5, active_event_c2[0]] = active_event_c2[5]
+
+                elif active_event[0] != active_event_list[i + 1][0]:  # if the next is not the same class
+                    if len(active_event_list_per_class) == 1:  # if no ov from the same class
+                        # a0----
+                        active_event_a0 = active_event_list_per_class[0]
+                        se_label[frame_ind, 0, active_event_a0[0]] = 1
+                        x_label[frame_ind, 0, active_event_a0[0]] = active_event_a0[2]
+                        y_label[frame_ind, 0, active_event_a0[0]] = active_event_a0[3]
+                        dist_label[frame_ind, 0, active_event_a0[0]] = active_event_a0[4] / 100.
+                        onscreen_label[frame_ind, 0, active_event_a0[0]] = active_event_a0[5]
+                    elif len(active_event_list_per_class) == 2:  # if ov with 2 sources from the same class
+                        # --b0--
+                        active_event_b0 = active_event_list_per_class[0]
+                        se_label[frame_ind, 1, active_event_b0[0]] = 1
+                        x_label[frame_ind, 1, active_event_b0[0]] = active_event_b0[2]
+                        y_label[frame_ind, 1, active_event_b0[0]] = active_event_b0[3]
+                        dist_label[frame_ind, 1, active_event_b0[0]] = active_event_b0[4] / 100.
+                        onscreen_label[frame_ind, 1, active_event_b0[0]] = active_event_b0[5]
+                        # --b1--
+                        active_event_b1 = active_event_list_per_class[1]
+                        se_label[frame_ind, 2, active_event_b1[0]] = 1
+                        x_label[frame_ind, 2, active_event_b1[0]] = active_event_b1[2]
+                        y_label[frame_ind, 2, active_event_b1[0]] = active_event_b1[3]
+                        dist_label[frame_ind, 2, active_event_b1[0]] = active_event_b1[4] / 100.
+                        onscreen_label[frame_ind, 2, active_event_b1[0]] = active_event_b1[5]
+                    else:  # if ov with more than 2 sources from the same class
+                        # ----c0
+                        active_event_c0 = active_event_list_per_class[0]
+                        se_label[frame_ind, 3, active_event_c0[0]] = 1
+                        x_label[frame_ind, 3, active_event_c0[0]] = active_event_c0[2]
+                        y_label[frame_ind, 3, active_event_c0[0]] = active_event_c0[3]
+                        dist_label[frame_ind, 3, active_event_c0[0]] = active_event_c0[4] / 100.
+                        onscreen_label[frame_ind, 3, active_event_c0[0]] = active_event_c0[5]
+                        # ----c1
+                        active_event_c1 = active_event_list_per_class[1]
+                        se_label[frame_ind, 4, active_event_c1[0]] = 1
+                        x_label[frame_ind, 4, active_event_c1[0]] = active_event_c1[2]
+                        y_label[frame_ind, 4, active_event_c1[0]] = active_event_c1[3]
+                        dist_label[frame_ind, 4, active_event_c1[0]] = active_event_c1[4] / 100.
+                        onscreen_label[frame_ind, 4, active_event_c1[0]] = active_event_c1[5]
+                        # ----c2
+                        active_event_c2 = active_event_list_per_class[2]
+                        se_label[frame_ind, 5, active_event_c2[0]] = 1
+                        x_label[frame_ind, 5, active_event_c2[0]] = active_event_c2[2]
+                        y_label[frame_ind, 5, active_event_c2[0]] = active_event_c2[3]
+                        dist_label[frame_ind, 5, active_event_c2[0]] = active_event_c2[4] / 100.
+                        onscreen_label[frame_ind, 5, active_event_c2[0]] = active_event_c2[5]
+                    active_event_list_per_class = []
+
+    label_mat = np.stack((se_label, x_label, y_label, dist_label, onscreen_label), axis=2)  # [nb_frames, 6, 5(act+XY+dist+onscreen), max_classes]
+    return label_mat
+
+
+def convert_polar_to_cartesian(input_dict):
+    output_dict = {}
+    for frame_idx in input_dict.keys():
+        if frame_idx not in output_dict:
+            output_dict[frame_idx] = []
+        for tmp_val in input_dict[frame_idx]:
+            azi_rad = tmp_val[2]*np.pi/180
+            x = np.cos(azi_rad)
+            y = np.sin(azi_rad)
+            output_dict[frame_idx].append(tmp_val[0:2] + [x, y] + tmp_val[3:])
+    return output_dict
+
+
+def convert_cartesian_to_polar(input_dict):
+    output_dict = {}
+    for frame_idx in input_dict.keys():
+        if frame_idx not in output_dict:
+            output_dict[frame_idx] = []
+        for tmp_val in input_dict[frame_idx]:
+            x = tmp_val[2]
+            y = tmp_val[3]
+            azi_rad = np.arctan2(y, x)
+            azimuth = azi_rad * 180 / np.pi
+            output_dict[frame_idx].append(tmp_val[0:2] + [azimuth] + tmp_val[4:])
+    return output_dict
